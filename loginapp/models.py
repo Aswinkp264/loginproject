92c66438a8623eaa2704bdd453dac02c36481618
aswinkp <kpaswin264@gmail.com>
2024-04-25 15:28:20 +0530

    user

--- loginapp/models.py
+++ loginapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.\\
+    
+class User(AbstractUser):
+    is_user = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default = False)
+    phone = models.CharField(max_length=100, null=True,blank=True)
+    address = models.TextField(null=True, blank=True) 
 
-# Create your models here.
+
